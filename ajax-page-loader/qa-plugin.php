2671cc664527fb3cae7e552fdaefc0b54b37b96f
Enolez <60704849+Enolez@users.noreply.github.com>
2020-02-29 11:13:04 +0600

    Updated File Name

--- ajax-page-loader/qa-plugin.php
+++ ajax-page-loader/qa-plugin.php
@@ -1,13 +1,13 @@
 <?php
 /*
-	Plugin Name: Ajax Page Loader
-	Plugin URI: http://TowhidN.com
-	Plugin Description: Loads Q2A pages using Ajax Technology to provide faster & modern pageload
+	Plugin Name: Enolez Page Loader
+	Plugin URI: https://github.com/Enolez/Ajax-Page-Loader/
+	Plugin Description: Loads Enolez pages using Enolez Page Loader to provide faster & modern pageload
 	Plugin Update Check URI: https://raw.githubusercontent.com/Towhidn/Ajax-Page-Loader/master/ajax-page-loader/qa-plugin.php
 	Plugin Version: 1.0.2
 	Plugin Date: 
-	Plugin Author: QA-Themes.com
-	Plugin Author URI: http://QA-Themes.com
+	Plugin Author: Masud
+	Plugin Author URI: https://www.enolez.com/আব্দুল্লাহ+আল+মাসুদ
 	Plugin License: GPLv2
 	Plugin Minimum Question2Answer Version: 1.5
 	Plugin Update Check URI:
@@ -20,5 +20,5 @@ if (!defined('QA_VERSION')) { // don't allow this page to be requested directly 
 }
 
 
-qa_register_plugin_layer('qa-ajax-layer.php', 'APL - Ajax Layer');
-qa_register_plugin_module('module', 'qa-ajax-admin-form.php', 'qa_ajax_admin_form', 'Ajax Page Loader');
+qa_register_plugin_layer('qa-ajax-layer.php', 'APL - Enolez Layer');
+qa_register_plugin_module('module', 'qa-ajax-admin-form.php', 'qa_ajax_admin_form', 'Enolez Page Loader');
